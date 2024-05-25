95b89ddb3c8da9a47c53da69defb97e389f15496
Ripple <108845456+Rippleglen@users.noreply.github.com>
2024-05-25 04:23:52 +0100

    Update ipcconstants.js
    Updated ipcconstants to now use RCG OAuth.

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45' // 4da45d88-11be-47e7-8ea7-f3b7023b9302 -- RCG Launcher Client ID
+exports.AZURE_CLIENT_ID = '4da45d88-11be-47e7-8ea7-f3b7023b9302' // 4da45d88-11be-47e7-8ea7-f3b7023b9302 -- RCG Launcher Client ID
 // SEE NOTE ABOVE.
 
 
